37107636b9a5a91b1658d63f7e2d3b932e6e8fca
V.Vranceanu <vsvladimir93@gmail.com>
2020-02-25 23:28:03 +0200

    Add create/connect methods

--- src/main/java/ChatSocket.java
+++ src/main/java/ChatSocket.java
@@ -10,42 +10,56 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import javax.websocket.Session;
 
-@ServerEndpoint("/chat/{username}")
+@ServerEndpoint("/chat/{params}")
 @ApplicationScoped
 public class ChatSocket {
 
-    Map<String, Session> sessions = new ConcurrentHashMap<>();
+    String field = "";
+    Map<String, Game> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("username") String username) {
-        sessions.put(username, session);
-        broadcast("User " + username + " joined");
+    public void onOpen(Session session, @PathParam("params") String params) {
+        String[] paramArray = params.split("&");
+        Game game = null;
+        if (!sessions.containsKey(paramArray[0])) {
+            game = new Game();
+            game.createGame(paramArray[0], paramArray[1], session);
+            sessions.put(paramArray[0], game);
+        } else {
+            game = sessions.get(paramArray[0]);
+            game.connectToGame(paramArray[0], paramArray[1], session);
+        }
+
+        broadcast(game);
     }
 
     @OnClose
     public void onClose(Session session, @PathParam("username") String username) {
-        sessions.remove(username);
-        broadcast("User " + username + " left");
+        // sessions.remove(username);
+        // broadcast("User " + username + " left");
     }
 
     @OnError
     public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
-        sessions.remove(username);
-        broadcast("User " + username + " left on error: " + throwable);
+        // sessions.remove(username);
+        // broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
     public void onMessage(String message, @PathParam("username") String username) {
-        broadcast(">> " + username + ": " + message);
+        // broadcast(">> " + username + ": " + message);
     }
 
-    private void broadcast(String message) {
-        sessions.values().forEach(s -> {
-            s.getAsyncRemote().sendObject(message, result ->  {
-                if (result.getException() != null) {
-                    System.out.println("Unable to send message: " + result.getException());
-                }
-            });
+    private void broadcast(Game game) {
+        game.playerX.getSession().getAsyncRemote().sendObject(game.info(), result ->  {
+                    if (result.getException() != null) {
+                        System.out.println("Unable to send message: " + result.getException());
+                    }
+                });
+        game.playerO.getSession().getAsyncRemote().sendObject(game.info(), result ->  {
+            if (result.getException() != null) {
+                System.out.println("Unable to send message: " + result.getException());
+            }
         });
     }
 
